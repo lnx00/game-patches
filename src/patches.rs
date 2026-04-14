6485bb16f70c0d908f0ec9a7dec1c6e994084d34
lnx00 <34384416+lnx00@users.noreply.github.com>
2026-04-14 02:05:35 +0200

    Added uniform camera speed patch

--- src/patches.rs
+++ src/patches.rs
@@ -1,9 +1,11 @@
 use std::sync::MutexGuard;
 
 use disable_camera_smoothing::DisableCameraSmoothing;
+use uniform_camera_speed::UniformCameraSpeed;
 //use mouse_sensitivity_fix::MouseSensitivityFix;
 
 pub mod disable_camera_smoothing;
+pub mod uniform_camera_speed;
 //pub mod mouse_sensitivity_fix;
 
 pub trait Patch {
@@ -17,6 +19,9 @@ pub fn run_all_patches() -> Result<(), String> {
     DisableCameraSmoothing::inst().apply()?;
     println!("- DisableCameraSmoothing applied");
 
+    UniformCameraSpeed::inst().apply()?;
+    println!("- UniformCameraSpeed applied");
+
     /*MouseSensitivityFix::inst().apply()?;
     println!("- MouseSensitivityFix applied");*/
 
@@ -26,6 +31,9 @@ pub fn run_all_patches() -> Result<(), String> {
 pub fn disable_all_patches() -> Result<(), String> {
     DisableCameraSmoothing::inst().revert()?;
     println!("- DisableCameraSmoothing reverted");
+
+    UniformCameraSpeed::inst().revert()?;
+    println!("- UniformCameraSpeed reverted");
 
     /*MouseSensitivityFix::inst().revert()?;
     println!("- DisableCameraSmoothing reverted");*/
--- src/patches/uniform_camera_speed.rs
+++ src/patches/uniform_camera_speed.rs
@@ -5,40 +5,39 @@ use crate::utils;
 use super::Patch;
 
 /*
-    The game loads a smoothing value of 0.05 by default. We can disable
-    smoothing by setting the register holding this value (xmm4) to zero.
+    The game uses factors 200 (x-axis) and 105 (y-axis) for the camera speed.
+    We can force a uniform 1:1 speed by loading the same factor for both axis.
 */
 
-pub struct DisableCameraSmoothing {
+pub struct UniformCameraSpeed {
     target_address: usize,
-    original_bytes: Box<[u8; 10]>,
+    original_bytes: Box<[u8; 8]>,
 }
 
-static INSTANCE: LazyLock<Mutex<DisableCameraSmoothing>> = LazyLock::new(|| {
+static INSTANCE: LazyLock<Mutex<UniformCameraSpeed>> = LazyLock::new(|| {
     let game_module = libmem::find_module("ShadowOfMordor.exe").unwrap();
     let target_address = unsafe {
-        libmem::sig_scan("84 C0 75 ? F3 0F 10 25", game_module.base, game_module.size)
+        libmem::sig_scan("F3 0F 10 0D ? ? ? ? F3 0F 10 15 ? ? ? ? EB ? F3 0F 10 54 24", game_module.base, game_module.size)
             .ok_or("signature not found")
             .unwrap()
     };
 
     let original_bytes = unsafe { libmem::read_memory::<_>(target_address) };
 
-    Mutex::new(DisableCameraSmoothing {
+    Mutex::new(UniformCameraSpeed {
         target_address,
         original_bytes: Box::new(original_bytes),
     })
 });
 
-impl Patch for DisableCameraSmoothing {
+impl Patch for UniformCameraSpeed {
     fn inst() -> MutexGuard<'static, Self> {
         INSTANCE.lock().unwrap()
     }
 
     fn apply(&mut self) -> Result<(), String> {
-        let patch_bytes: [u8; 10] = [
-            0x66, 0x0F, 0xEF, 0xE4, // pxor xmm4, xmm4
-            0x90, 0x90, 0x90, 0x90, 0x90, 0x90 // nop
+        let patch_bytes: [u8; 8] = [
+            0xF3, 0x0F, 0x10, 0x0D, 0x4C, 0xC1, 0x29, 0x01 // movss xmm1, dword ptr cs:const_flt_105
         ];
         utils::patch_bytes(self.target_address, &patch_bytes)?;
 

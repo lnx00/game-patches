d92085e471295e5f98a56f8a64530bb9fb3feaf0
lnx00 <34384416+lnx00@users.noreply.github.com>
2026-04-29 00:22:23 +0200

    Implemented experimental integrity check search

--- acu-patches/src/game/integrity.rs
+++ acu-patches/src/game/integrity.rs
@@ -1,5 +1,7 @@
 use std::{
+    convert::TryFrom,
     ffi::c_void,
+    ops::Range,
     sync::{LazyLock, Mutex, MutexGuard},
 };
 
@@ -8,11 +10,13 @@ use windows::{
     Win32::{
         Foundation::HANDLE,
         System::{
+            Diagnostics::Debug::{IMAGE_FILE_HEADER, IMAGE_SECTION_HEADER},
             LibraryLoader::{GetModuleHandleA, GetProcAddress},
+            SystemServices::{IMAGE_DOS_HEADER, IMAGE_DOS_SIGNATURE, IMAGE_NT_SIGNATURE},
             Threading::{OpenThread, THREAD_ALL_ACCESS, TerminateThread},
         },
     },
-    core::s,
+    core::{PCSTR, s},
 };
 
 type CreateThreadFn = unsafe extern "system" fn(
@@ -24,7 +28,116 @@ type CreateThreadFn = unsafe extern "system" fn(
     lp_thread_id: *mut u32,
 ) -> HANDLE;
 
-const INTEGRITY_THREAD_START_ADDRESS: usize = 0x14275DE50;
+#[cfg(target_pointer_width = "64")]
+use windows::Win32::System::Diagnostics::Debug::IMAGE_NT_HEADERS64 as IMAGE_NT_HEADERS;
+
+#[cfg(target_pointer_width = "32")]
+use windows::Win32::System::Diagnostics::Debug::IMAGE_NT_HEADERS32 as IMAGE_NT_HEADERS;
+
+pub fn find_section_address_range(section_name: &str) -> Option<Range<usize>> {
+    let mut target_name_bytes = [0u8; 8];
+    let bytes = section_name.as_bytes();
+    let len = bytes.len().min(8);
+    target_name_bytes[..len].copy_from_slice(&bytes[..len]);
+
+    unsafe {
+        // Base address
+        let h_module = GetModuleHandleA(PCSTR::null()).ok()?;
+        if h_module.is_invalid() {
+            return None;
+        }
+        let base_addr = h_module.0 as *const u8;
+
+        // DOS Header
+        let dos_header = &*(base_addr as *const IMAGE_DOS_HEADER);
+        if dos_header.e_magic != IMAGE_DOS_SIGNATURE {
+            return None;
+        }
+
+        // NT Headers
+        let nt_headers_ptr = base_addr.add(dos_header.e_lfanew as usize) as *const IMAGE_NT_HEADERS;
+        let nt_headers = &*nt_headers_ptr;
+
+        if nt_headers.Signature != IMAGE_NT_SIGNATURE {
+            return None;
+        }
+
+        // Section headers start
+        let optional_header_offset = 4 + std::mem::size_of::<IMAGE_FILE_HEADER>();
+        let section_headers_ptr = (nt_headers_ptr as *const u8)
+            .add(optional_header_offset)
+            .add(nt_headers.FileHeader.SizeOfOptionalHeader as usize)
+            as *const IMAGE_SECTION_HEADER;
+
+        // Convert section headers
+        let num_sections = nt_headers.FileHeader.NumberOfSections as usize;
+        let sections = std::slice::from_raw_parts(section_headers_ptr, num_sections);
+
+        // Find section
+        for section in sections {
+            if section.Name == target_name_bytes {
+                let virtual_size = section.Misc.VirtualSize as usize;
+
+                let start_address = (base_addr as usize) + section.VirtualAddress as usize;
+                let end_address = start_address + virtual_size;
+
+                return Some(start_address..end_address);
+            }
+        }
+    }
+
+    None
+}
+
+static INTEGRITY_SECTION_RANGE: LazyLock<Option<(usize, usize)>> = LazyLock::new(|| {
+    let target = find_section_address_range(".UBX0")?;
+    println!("found section @ {} - {}", target.start, target.end);
+    return Some((target.start, target.end));
+});
+
+/// Extract the jump target address
+fn jump_target_address(inst: &libmem::Inst) -> Option<usize> {
+    let next_address = inst.address as i64 + inst.bytes.len() as i64;
+
+    let target = match inst.bytes.as_slice() {
+        [0xE9, displacement @ ..] if displacement.len() == 4 => {
+            let displacement = i32::from_le_bytes(displacement.try_into().ok()?) as i64;
+            next_address.checked_add(displacement)?
+        }
+        [0xEB, displacement] => {
+            let displacement = i8::from_le_bytes([*displacement]) as i64;
+            next_address.checked_add(displacement)?
+        }
+        _ => return None,
+    };
+
+    usize::try_from(target).ok()
+}
+
+/// Analyzes the thread start code and checks if it is the integrity thread
+fn analyze_thread_start(start_address: usize) -> Option<bool> {
+    unsafe {
+        println!("analyzing thread {:X}...", start_address);
+
+        let section_range = (*INTEGRITY_SECTION_RANGE)?;
+        println!("- section range: {}, {}", section_range.0, section_range.1);
+
+        let inst = libmem::disassemble(start_address)?;
+
+        if inst.mnemonic.to_lowercase() != "jmp" {
+            println!("- first inst was not a jump");
+            return Some(false);
+        }
+
+        let target_addr = jump_target_address(&inst)?;
+        println!("- jmp target addr: {}", target_addr);
+
+        let in_range = target_addr >= section_range.0 && target_addr <= section_range.1;
+        println!("verdict for thread {:X}: {}", start_address, in_range);
+
+        return Some(in_range);
+    }
+}
 
 fn check_thread(thread_id: u32) -> Result<bool, String> {
     unsafe {
@@ -50,7 +163,7 @@ fn check_thread(thread_id: u32) -> Result<bool, String> {
             ));
         }
 
-        if thread_start_address == INTEGRITY_THREAD_START_ADDRESS {
+        if analyze_thread_start(thread_start_address) == Some(true) {
             TerminateThread(thread_handle, 0x0).map_err(|_| "failed to terminate thread")?;
             return Ok(true);
         }
@@ -150,7 +263,7 @@ impl IntegrityHook {
     ) -> HANDLE {
         let mut lp_start_address = lp_start_address;
 
-        if lp_start_address as usize == INTEGRITY_THREAD_START_ADDRESS {
+        if analyze_thread_start(lp_start_address as usize) == Some(true) {
             lp_start_address = Self::empty_thread as *mut c_void;
             println!("CreateThread: prevented integrity check thread creation");
         }

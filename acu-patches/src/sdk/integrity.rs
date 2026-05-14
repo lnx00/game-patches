31fa0f86344a0f68b8489a6cf26c0e6f80757f58
lnx00 <34384416+lnx00@users.noreply.github.com>
2026-05-15 00:55:22 +0200

    Cleaned up integrity check bypass

--- acu-patches/src/sdk/integrity.rs
+++ acu-patches/src/sdk/integrity.rs
@@ -1,6 +1,5 @@
 use std::{
     collections::HashMap,
-    convert::TryFrom,
     ffi::c_void,
     ops::Range,
     sync::{
@@ -16,22 +15,20 @@ use windows::{
     Win32::{
         Foundation::{CloseHandle, HANDLE},
         System::{
-            Diagnostics::Debug::{IMAGE_FILE_HEADER, IMAGE_SECTION_HEADER},
             LibraryLoader::{GetModuleHandleA, GetProcAddress},
-            SystemServices::{IMAGE_DOS_HEADER, IMAGE_DOS_SIGNATURE, IMAGE_NT_SIGNATURE},
             Threading::{OpenThread, THREAD_ALL_ACCESS, TerminateThread},
         },
     },
-    core::{PCSTR, s},
+    core::s,
 };
 
 use framework::utils::{self, WaitLock};
 
-#[cfg(target_pointer_width = "64")]
-use windows::Win32::System::Diagnostics::Debug::IMAGE_NT_HEADERS64 as IMAGE_NT_HEADERS;
-
-#[cfg(target_pointer_width = "32")]
-use windows::Win32::System::Diagnostics::Debug::IMAGE_NT_HEADERS32 as IMAGE_NT_HEADERS;
+/*
+    The main integrity check thread immediately jumps into the VMP section (.UBX0).
+    We can thus identify the MIC by analyzing the first instruction of every new
+    thread to see if it is a jmp/call into said section.
+*/
 
 type CreateThreadFn = unsafe extern "system" fn(
     lp_thread_attributes: *mut c_void,
@@ -44,63 +41,8 @@ type CreateThreadFn = unsafe extern "system" fn(
 
 static ORIG_CREATE_THREAD: OnceLock<CreateThreadFn> = OnceLock::new();
 
-pub fn find_section_address_range(section_name: &str) -> Option<Range<usize>> {
-    let mut target_name_bytes = [0u8; 8];
-    let bytes = section_name.as_bytes();
-    let len = bytes.len().min(8);
-    target_name_bytes[..len].copy_from_slice(&bytes[..len]);
-
-    unsafe {
-        // Base address
-        let h_module = GetModuleHandleA(PCSTR::null()).ok()?;
-        if h_module.is_invalid() {
-            return None;
-        }
-        let base_addr = h_module.0 as *const u8;
-
-        // DOS Header
-        let dos_header = &*(base_addr as *const IMAGE_DOS_HEADER);
-        if dos_header.e_magic != IMAGE_DOS_SIGNATURE {
-            return None;
-        }
-
-        // NT Headers
-        let nt_headers_ptr = base_addr.add(dos_header.e_lfanew as usize) as *const IMAGE_NT_HEADERS;
-        let nt_headers = &*nt_headers_ptr;
-
-        if nt_headers.Signature != IMAGE_NT_SIGNATURE {
-            return None;
-        }
-
-        // Section headers start
-        let optional_header_offset = 4 + std::mem::size_of::<IMAGE_FILE_HEADER>();
-        let section_headers_ptr = (nt_headers_ptr as *const u8)
-            .add(optional_header_offset)
-            .add(nt_headers.FileHeader.SizeOfOptionalHeader as usize)
-            as *const IMAGE_SECTION_HEADER;
-
-        // Convert section headers
-        let num_sections = nt_headers.FileHeader.NumberOfSections as usize;
-        let sections = std::slice::from_raw_parts(section_headers_ptr, num_sections);
-
-        // Find section
-        for section in sections {
-            if section.Name == target_name_bytes {
-                let virtual_size = section.Misc.VirtualSize as usize;
-
-                let start_address = (base_addr as usize) + section.VirtualAddress as usize;
-                let end_address = start_address + virtual_size;
-
-                return Some(start_address..end_address);
-            }
-        }
-    }
-
-    None
-}
-
 static INTEGRITY_SECTION_RANGE: LazyLock<Option<Range<usize>>> =
-    LazyLock::new(|| find_section_address_range(".UBX0"));
+    LazyLock::new(|| utils::platform::find_section_address_range(".UBX0"));
 
 static INTEGRITY_THREAD_VERDICTS: LazyLock<RwLock<HashMap<usize, bool>>> =
     LazyLock::new(|| RwLock::new(HashMap::new()));
--- framework/src/utils/platform.rs
+++ framework/src/utils/platform.rs
@@ -1,5 +1,9 @@
+use std::ops::Range;
+
 use windows::Win32::System::Console::{AllocConsole, FreeConsole, SetConsoleTitleW};
-use windows::Win32::System::Diagnostics::Debug::IMAGE_NT_HEADERS64;
+use windows::Win32::System::Diagnostics::Debug::{
+    IMAGE_FILE_HEADER, IMAGE_NT_HEADERS64, IMAGE_SECTION_HEADER,
+};
 use windows::Win32::System::LibraryLoader::GetModuleHandleW;
 use windows::Win32::System::SystemServices::{
     IMAGE_DOS_HEADER, IMAGE_DOS_SIGNATURE, IMAGE_NT_SIGNATURE,
@@ -78,4 +82,60 @@ pub fn get_time_date_stamp() -> Option<u32> {
 
         Some(nt_headers.FileHeader.TimeDateStamp)
     }
+}
+
+pub fn find_section_address_range(section_name: &str) -> Option<Range<usize>> {
+    let mut target_name_bytes = [0u8; 8];
+    let bytes = section_name.as_bytes();
+    let len = bytes.len().min(8);
+    target_name_bytes[..len].copy_from_slice(&bytes[..len]);
+
+    unsafe {
+        // Base address
+        let h_module = GetModuleHandleW(PCWSTR::null()).ok()?;
+        if h_module.is_invalid() {
+            return None;
+        }
+        let base_addr = h_module.0 as *const u8;
+
+        // DOS Header
+        let dos_header = &*(base_addr as *const IMAGE_DOS_HEADER);
+        if dos_header.e_magic != IMAGE_DOS_SIGNATURE {
+            return None;
+        }
+
+        // NT Headers
+        let nt_headers_ptr =
+            base_addr.add(dos_header.e_lfanew as usize) as *const IMAGE_NT_HEADERS64;
+        let nt_headers = &*nt_headers_ptr;
+
+        if nt_headers.Signature != IMAGE_NT_SIGNATURE {
+            return None;
+        }
+
+        // Section headers start
+        let optional_header_offset = 4 + std::mem::size_of::<IMAGE_FILE_HEADER>();
+        let section_headers_ptr = (nt_headers_ptr as *const u8)
+            .add(optional_header_offset)
+            .add(nt_headers.FileHeader.SizeOfOptionalHeader as usize)
+            as *const IMAGE_SECTION_HEADER;
+
+        // Convert section headers
+        let num_sections = nt_headers.FileHeader.NumberOfSections as usize;
+        let sections = std::slice::from_raw_parts(section_headers_ptr, num_sections);
+
+        // Find section
+        for section in sections {
+            if section.Name == target_name_bytes {
+                let virtual_size = section.Misc.VirtualSize as usize;
+
+                let start_address = (base_addr as usize) + section.VirtualAddress as usize;
+                let end_address = start_address + virtual_size;
+
+                return Some(start_address..end_address);
+            }
+        }
+    }
+
+    None
 }
